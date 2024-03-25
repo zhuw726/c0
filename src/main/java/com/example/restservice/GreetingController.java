2cf13a83c92e030e2e3ee8c6ec18a6bb593824a1
Wenjian Zhu <zhuwj726@gmail.com>
2024-03-25 14:42:37 +0800

    init

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String TEMPLATE = "Hello, %s!";
+	private static final String TEMPLATE = "Hello ++ , %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
